2abaf7a5f1e1342fd8ec3a93b31a9af628efeb7f
sandeep sharma <sandeep243123@gmail.com>
2022-10-16 14:47:53 +0530

    second commit

---
+++ kotlin/test/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println(2+6)
+    println(true)
+
+}
---
+++ kotlin/test/src/main/kotlin/variable.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a=11
+    var b=2
+    println(a+b)
+    println(a-b)
+    println(a.toFloat()/b)
+    println(a%b)
+
+
+
+    println(a)
+}
